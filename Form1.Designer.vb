a1947ac75c9de38a0e832b7f236f99e5992ac0ac
AlZaj <alzaj@yahoo.com>
2012-01-10 14:58:57 +0100

    datamodel with connection and temp data interface

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.closeForm = New System.Windows.Forms.Button()
+        Me.saveChanges = New System.Windows.Forms.Button()
+        Me.tasksGridView = New System.Windows.Forms.DataGridView()
+        CType(Me.tasksGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'closeForm
+        '
+        Me.closeForm.Location = New System.Drawing.Point(197, 227)
+        Me.closeForm.Name = "closeForm"
+        Me.closeForm.Size = New System.Drawing.Size(75, 23)
+        Me.closeForm.TabIndex = 0
+        Me.closeForm.Text = "Close"
+        Me.closeForm.UseVisualStyleBackColor = True
+        '
+        'saveChanges
+        '
+        Me.saveChanges.Location = New System.Drawing.Point(116, 226)
+        Me.saveChanges.Name = "saveChanges"
+        Me.saveChanges.Size = New System.Drawing.Size(75, 23)
+        Me.saveChanges.TabIndex = 1
+        Me.saveChanges.Text = "Save"
+        Me.saveChanges.UseVisualStyleBackColor = True
+        '
+        'tasksGridView
+        '
+        Me.tasksGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.tasksGridView.Location = New System.Drawing.Point(13, 13)
+        Me.tasksGridView.Name = "tasksGridView"
+        Me.tasksGridView.Size = New System.Drawing.Size(512, 192)
+        Me.tasksGridView.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 262)
+        Me.Controls.Add(Me.tasksGridView)
+        Me.Controls.Add(Me.saveChanges)
+        Me.Controls.Add(Me.closeForm)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.tasksGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents closeForm As System.Windows.Forms.Button
+    Friend WithEvents saveChanges As System.Windows.Forms.Button
+    Friend WithEvents tasksGridView As System.Windows.Forms.DataGridView
 
 End Class
